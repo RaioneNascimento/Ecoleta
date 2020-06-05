dd3d7f9312b11bb643924dbaadc50079588fdff7
Raione Nascimento <56979345+RaioneNascimento@users.noreply.github.com>
2020-06-04 21:28:57 -0300

    create-point
    Criação da tela Create-point, juntamente com as configurações do JavaScript e CSS de responsividade.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,3 +1,4 @@
+// Função API de localidade no site do IBGA   
 function populateUFs() {
   const ufSelect = document.querySelector("select[name=uf]")
 
@@ -23,16 +24,19 @@ function getCities(event) {
   const indexOfSelectedState = event.target.selectedIndex
   stateInput.value = event.target.options[indexOfSelectedState].text
 
-  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+  citySelect.disabled = true
 
   fetch(url)
   .then( res => res.json() ) 
   .then( cities => { 
 
     for( const city of cities ) {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
     }
-    
+
     citySelect.disabled = false
    })
 }
@@ -40,3 +44,48 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+  // Itens de coleta
+  // pegar todos os li's
+
+  const itemsToCollect = document.querySelectorAll(".items-grid li")
+  
+  for (const item of itemsToCollect) {
+    item.addEventListener("click",handleSelectedItem)
+  }
+  
+  const collectedItems = document.querySelector("input[name=items]")
+
+  let selectedItems = []
+
+  function handleSelectedItem(event) {
+    const itemLi = event.target
+    
+    // adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id
+
+    // verificar se existem itens selecionados, se sim
+    // pegar os items selecionados
+    const alreadySelected = selectedItems.findIndex( item => {
+      const itemFound = item == itemId //isso será true ou false
+      return itemFound
+    })
+
+    // se já estiver selecionado
+    if ( alreadySelected >= 0 ) {
+      //tirar da selecao
+      const filteredItems = selectedItems.filter( item => {
+        const itemIsDifferent = item != itemId
+        return itemIsDifferent
+      })
+
+      selectedItems = filteredItems
+    } else {
+      // se não tiver selecionado
+      selectedItems.push(itemId)
+    }
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+  } 
